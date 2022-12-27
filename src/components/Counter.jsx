42f02e6003281b648248322f963e200662816802
jgonzalez-89 <kbcerverjo@gmail.com>
2022-12-27 22:29:01 +0100

    Create clear Button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,32 +1,40 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
 
 function Counter() {
-  const [numbers, setNumbers] = useState("0000")
+  const [numbers, setNumbers] = useState("0000");
 
   const fill = (num) => {
     let n = parseInt(num) + 1;
     let l = n.toString().length;
     let zeros = 4 - l;
-    return "0".repeat(zeros) + n
-  }
+    return "0".repeat(zeros) + n;
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setNumbers(prev => fill(prev));
+      setNumbers((prev) => fill(prev));
     }, 1000);
     return () => clearInterval(interval);
-  }, [])
+  }, []);
 
+  const setZero = () => {
+    setNumbers("0000");
+  };
 
   return (
-  
-  <div className='container'>{numbers.split("").map((number, index) => {
-          return (
-              <div className='box' key={index}>{number}</div>
-          )
-        })}
-  </div>
-  )
+    <div className="container">
+      {numbers.split("").map((number, index) => {
+        return (
+          <div className="box" key={index}>
+            {number}
+          </div>
+        );
+      })}
+
+      <button className="btn" type="button" onClick={() => setZero()}>
+        Clear
+      </button>
+    </div>
+  );
 }
-export default Counter
+export default Counter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Buttons from './components/Buttons'
-import Counter from './components/Counter'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counter from "./components/Counter";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Counter />
-    <Buttons />
   </React.StrictMode>
-)
+);
